d861e52026a358c80cac1d0af01d5c01ffe5c836
Andrea F. Daniele <afdaniele@ttic.edu>
2022-10-13 16:43:31 -0400

    minor

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description() -> str:
 
 setup(
     name="pydock",
-    version="0.0.1",
+    version="0.0.0",
     description="A decent Python wrapper for Docker CLI",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
