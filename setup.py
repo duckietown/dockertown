dcc92a9edce4cdffdf2560e282548d9290e98839
Andrea F. Daniele <afdaniele@duckietown.com>
2023-04-20 19:16:29 -0400

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description() -> str:
 
 setup(
     name="dockertown",
-    version="0.2.1",
+    version="0.2.2",
     description="A decent Python wrapper for Docker CLI",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
